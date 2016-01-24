74feb8b085fc8a7cb410cc9ebe1be5f09aff3e61
cyang001 <cyang001@citymail.cuny.edu>
2016-01-23 23:24:11 -0500

    Complete basic functionalities

--- ghost_starter/app/src/main/java/com/google/engedu/ghost/GhostActivity.java
+++ ghost_starter/app/src/main/java/com/google/engedu/ghost/GhostActivity.java
@@ -2,25 +2,53 @@ package com.google.engedu.ghost;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.util.Random;
 
 
 public class GhostActivity extends ActionBarActivity {
     private static final String COMPUTER_TURN = "Computer's turn";
     private static final String USER_TURN = "Your turn";
+    private static final String COMPUTER_VICTORY = "Computer victory";
+    private static final String USER_VICTORY = "User victory";
+
+
     private GhostDictionary dictionary;
     private boolean userTurn = false;
     private Random random = new Random();
+
+    private TextView ghostTextView;
+    private TextView gameStatusTextView;
+
+    private Button challengeButton;
+
+    private String wordFragment = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ghost);
+
+        ghostTextView = (TextView) findViewById(R.id.ghostText);
+        gameStatusTextView = (TextView) findViewById(R.id.gameStatus);
+
+        challengeButton = (Button) findViewById(R.id.challengeButton);
+
+        try {
+            dictionary = new SimpleDictionary(getAssets().open("words.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         onStart(null);
     }
 
@@ -47,10 +75,38 @@ public class GhostActivity extends ActionBarActivity {
     }
 
     private void computerTurn() {
-        TextView label = (TextView) findViewById(R.id.gameStatus);
         // Do computer turn stuff then make it the user's turn again
+        if(wordFragment.length() >= 4) {
+            if (dictionary.isWord(wordFragment)) {
+
+                // User player has completed a word
+                gameStatusTextView.setText(COMPUTER_VICTORY + "; you completed a word");
+
+                challengeButton.setEnabled(false);
+
+                return;
+            }
+        }
+
+        // Get a new possible longer word
+        String possibleWord = dictionary.getAnyWordStartingWith(wordFragment);
+
+        if(possibleWord == null){
+            // Word doesn't exist
+            gameStatusTextView.setText(COMPUTER_VICTORY + "; not a valid prefix");
+
+            challengeButton.setEnabled(false);
+
+            return;
+        } else {
+            // Word does exist
+            wordFragment = possibleWord.substring(0, wordFragment.length()+1);
+            ghostTextView.setText(wordFragment);
+        }
+
+
         userTurn = true;
-        label.setText(USER_TURN);
+        gameStatusTextView.setText(USER_TURN);
     }
 
     /**
@@ -61,15 +117,61 @@ public class GhostActivity extends ActionBarActivity {
      */
     public boolean onStart(View view) {
         userTurn = random.nextBoolean();
-        TextView text = (TextView) findViewById(R.id.ghostText);
-        text.setText("");
-        TextView label = (TextView) findViewById(R.id.gameStatus);
+        ghostTextView.setText("");
+        wordFragment = "";
+
         if (userTurn) {
-            label.setText(USER_TURN);
+            gameStatusTextView.setText(USER_TURN);
         } else {
-            label.setText(COMPUTER_TURN);
+            gameStatusTextView.setText(COMPUTER_TURN);
             computerTurn();
         }
+
+        challengeButton.setEnabled(true);
+
         return true;
     }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event){
+        if(keyCode >= 29 && keyCode <= 54) {
+            // Key is a valid letter
+            wordFragment = String.format("%s%s", ghostTextView.getText(), String.valueOf(event.getDisplayLabel()).toLowerCase());
+            ghostTextView.setText(wordFragment);
+
+            // End user turn and begin computer's turn
+            gameStatusTextView.setText(COMPUTER_TURN);
+            userTurn = false;
+            computerTurn();
+            return true;
+        } else {
+            return super.onKeyUp(keyCode, event);
+        }
+    }
+
+    public void challenge(View view) {
+        if(wordFragment.length() >= 4){
+            if(dictionary.isWord(wordFragment)){
+                // Computer has finished a word
+                gameStatusTextView.setText(USER_VICTORY);
+            } else {
+                String possibleWord = dictionary.getAnyWordStartingWith(wordFragment);
+
+                if(possibleWord != null) {
+                    // Computer creates a fragment that is a prefix of a word from the dictionary
+                    gameStatusTextView.setText(String.format(COMPUTER_VICTORY + "; Possible word: %s", possibleWord));
+                } else {
+                    // Computer creates a bad fragment
+                    gameStatusTextView.setText(USER_VICTORY);
+
+                }
+            }
+
+            challengeButton.setEnabled(false);
+
+        } else {
+            gameStatusTextView.setText("You may only challenge a fragment that is at least 4 letters long!");
+        }
+    }
+
 }
--- ghost_starter/app/src/main/java/com/google/engedu/ghost/SimpleDictionary.java
+++ ghost_starter/app/src/main/java/com/google/engedu/ghost/SimpleDictionary.java
@@ -5,9 +5,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class SimpleDictionary implements GhostDictionary {
     private ArrayList<String> words;
+    private Random random = new Random();
 
     public SimpleDictionary(InputStream wordListStream) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(wordListStream));
@@ -27,11 +29,52 @@ public class SimpleDictionary implements GhostDictionary {
 
     @Override
     public String getAnyWordStartingWith(String prefix) {
-        return null;
+        if(prefix.isEmpty()){
+            return words.get(random.nextInt(words.size()));
+        } else {
+            return binarySearch(words, prefix);
+        }
     }
 
     @Override
     public String getGoodWordStartingWith(String prefix) {
         return null;
+    }
+
+    public String binarySearch(ArrayList<String> input, String prefix){
+        int length = input.size();
+        String current = input.get(length/2);
+        System.out.println(length);
+
+
+        if(current.startsWith(prefix))
+            return current;
+
+        if(!current.startsWith(prefix) && length == 1)
+            return null;
+
+        ArrayList<String> temp = new ArrayList<>();
+
+        int i = 0;
+        char[] prefixCharArray = prefix.toCharArray();
+        char[] currentCharArray = current.toCharArray();
+
+        // Find index of first non-matching character between prefix and current
+        while(i < prefixCharArray.length){
+            if(prefixCharArray[i] != currentCharArray[i]){
+                break;
+            }
+            i++;
+        }
+
+        if(prefixCharArray[i] < currentCharArray[i]){
+            // Check left
+            temp.addAll(input.subList(0, length/2));
+            return binarySearch(temp, prefix);
+        } else {
+            // Check right
+            temp.addAll(input.subList(length/2, length));
+            return binarySearch(temp, prefix);
+        }
     }
 }
