97b127956a00caefd92191242726c694f638bccc
cyang001 <cyang001@citymail.cuny.edu>
2016-02-06 17:48:39 -0500

    Add Puzzle8

---
+++ Puzzle8_starter/app/src/main/java/com/google/engedu/puzzle8/PuzzleActivity.java
@@ -0,0 +1,126 @@
+package com.google.engedu.puzzle8;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class PuzzleActivity extends AppCompatActivity {
+
+    private Bitmap imageBitmap = null;
+    private PuzzleBoardView boardView;
+    private Uri fileUri;
+    private final int IMAGE_REQUEST_CODE = 100;
+
+    private String mCurrentPhotoPath;
+    private BitmapFactory.Options options;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_puzzle);
+
+        RelativeLayout container = (RelativeLayout) findViewById(R.id.puzzle_container);
+        boardView = new PuzzleBoardView(this);
+
+        // Some setup of the view.
+        boardView.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
+        container.addView(boardView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_puzzle, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK) {
+            imageBitmap = (Bitmap) data.getExtras().get("data");
+            imageBitmap = BitmapFactory.decodeFile(mCurrentPhotoPath.substring(5));
+            boardView.initialize(imageBitmap, boardView);
+        }
+    }
+
+    public void dispatchTakePictureIntent(View view) {
+        Intent imageCaptureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (imageCaptureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                imageCaptureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+                startActivityForResult(imageCaptureIntent, IMAGE_REQUEST_CODE);
+            }
+        }
+    }
+
+    public void shuffleImage(View view) {
+        boardView.shuffle();
+    }
+
+    public void solve(View view) {
+      boardView.solve();
+
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+}
